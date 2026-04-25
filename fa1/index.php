1b54fd33dfd0bf1a28116440ff25a690214721bd
tutubee <vettatad@gmail.com>
2026-04-25 15:05:13 +0800

    update code

--- fa1/index.php
+++ fa1/index.php
@@ -7,32 +7,52 @@
 </head>
 <body>
 
+    <?php $name = "VHEA ERIKA TATAD"; 
+    $phone = "+63 912 345 6789";
+    $email = "tatadv@gmail.com"; 
+    $college = "Far Eastern University";
+    $highschool = "Quezon City Science High School"; ?>
+
     <div class="grid-container">
         <div class="contact">
-            <h1><?php echo "VHEA ERIKA TATAD" ?></h1>
-            <h3><?php echo "+63 912 345 6789" ?></h3>
-            <h3><?php echo "tatadv@gmail.com" ?></h3>
-        </div>
+            <div class="info">
+                <h1><?php echo $name ?></h1>
+                <h3><?php echo "Phone No.: " . $phone ?></h3>
+                <h3><?php echo "Email: " . $email ?></h3>
+            </div>
 
-        <hr>
+            <img src="profile.jpg" alt="Profile Picture">
+        </div>
         
         <div class="profile">
             <h2><?php echo "Profile" ?></h2>
+            <hr>
             <h3><?php echo "I am a dedicated student with a passion for learning and growth." ?></h3>
         </div>
 
         <div class="education">
             <h2><?php echo "Education" ?></h2>
-
-            <p><?php echo "Senior High School 2022 - 2024" ?></p>
-            <h3><?php echo "Quezon City Science High School" ?></h3>
-
-            <p><?php echo "Junior High School 2020 - 2023" ?></p>
-            <h3><?php echo "Quezon City Science High School" ?></h3>
+            <hr>
+            <ul>
+                <li>
+                    <h3><?php echo $college ?></h3>
+                    <p><?php echo "College 2024 - Present" ?></p>
+                </li>
+                <li>
+                    <h3><?php echo $highschool ?></h3>
+                    <p><?php echo "Senior High School 2022 - 2024" ?></p>
+                </li>
+                <li>
+                    <h3><?php echo $highschool ?></h3>
+                    <p><?php echo "Junior High School 2020 - 2023" ?></p>
+                </li>
+                
+            </ul>
         </div>
 
         <div class="skills">
             <h2><?php echo "Skills" ?></h2>
+            <hr>
             <ul>
                 <li><?php echo "Communication" ?></li>
                 <li><?php echo "Teamwork" ?></li>
@@ -41,6 +61,7 @@
         </div>
 
         <div class="references">
+            <hr>
             <h2><?php echo "References" ?></h2>
             <ul>
                 <li>
