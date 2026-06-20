3492df7eac570c49bc6f97d19d96edef34d950aa
tutubee <vettatad@gmail.com>
2026-06-20 15:16:07 +0800

    cookie

--- fa5/cookie.php
+++ fa5/cookie.php
@@ -1,36 +1,45 @@
-<?php
-        $fname = ["Juan", "Maria", "John"];
-        $mname = ["Amigo", "Isabella", "Mark"];
-        $lname = ["Dela Cruz", "Santos", "Espiritu"];
-
-        foreach ($fname as $key => $value) {
-            setcookie("fname", $value, time() + 10, "/");
-        }
-        foreach ($mname as $key => $value) {
-            setcookie("mname", $value, time() + 20, "/");
-        }
-        foreach ($lname as $key => $value) {
-            setcookie("lname", $value, time() + 30, "/");
-        }
-    ?>
-
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cookie</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-        
-        if (isset($_COOKIE["fname"]) && isset($_COOKIE["mname"]) && isset($_COOKIE["lname"])) {
-            echo "<h2>Cookie Data</h2>";
-            echo "<strong>First Name:</strong> " . $_COOKIE["fname"] . "<br>";
-            echo "<strong>Middle Name:</strong> " . $_COOKIE["mname"] . "<br>";
-            echo "<strong>Last Name:</strong> " . $_COOKIE["lname"] . "<br>";
-        } else {
-            echo "Cookie is not set.";
-        }
-    ?>
+    <div class="cookiecontainer">
+        <form method="POST" action="cookie.php">
+            <label for="fname">First Name:</label>
+                <input type="text" name="fname" placeholder="Juan" required>
+                <br><br>
+                <label for="mname">Middle Name:</label>
+                <input type="text" name="mname" placeholder="Amigo" required>
+                <br><br>
+                <label for="lname">Last Name:</label>
+                <input type="text" name="lname" placeholder="Dela Cruz" required>
+                <br><br>
+            <input type="submit" name="submit" value="Set Cookie">
+        </form>
+        <?php
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $fname = $_POST['fname'];
+                $mname = $_POST['mname'];
+                $lname = $_POST['lname'];
+
+                setcookie("fname", $fname, time() +10, "/");
+                setcookie("mname", $mname, time() +20, "/");
+                setcookie("lname", $lname, time() +30, "/");
+            }
+
+            if (isset($_COOKIE["fname"]) && isset($_COOKIE["mname"]) && isset($_COOKIE["lname"])) {
+                echo "<br><br>" . "<h2>Cookie Data</h2>";
+                echo "<strong>First Name:</strong> " . $_COOKIE["fname"] . "<br>";
+                echo "<strong>Middle Name:</strong> " . $_COOKIE["mname"] . "<br>";
+                echo "<strong>Last Name:</strong> " . $_COOKIE["lname"] . "<br>";
+            } else {
+                echo "<br><br>" . "Cookie is not set.";
+            }
+        ?>
+    </div>
 </body>
 </html>
